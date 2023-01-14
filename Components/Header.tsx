fe505941d814879fcc46e86bdcda2b11dabefefe
Jeremy Smith <st.jeremy1@gmail.com>
2023-01-14 22:57:41 +0100

    fix

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -1,9 +1,14 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 
+interface Text{
+  
+}
 const Header = () => {
   return ( 
     <Box>
-      
+      <Box>
+        <Text>Welcome to Moviez!</Text>
+      </Box>
     </Box>
    );
 }
--- Components/MovieList.tsx
+++ Components/MovieList.tsx
@@ -1,8 +1,12 @@
 import { Box } from "@chakra-ui/react";
+import { useState } from "react";
 
 const MovieList = () => {
+  const [ movies, setMovies ] = useState([])
   return ( 
     <Box>
+      {movi}
+      
       
     </Box>
    );

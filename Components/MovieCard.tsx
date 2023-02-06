c80699622e2187e1e64fa8a9b5d0f42fd7660a0b
Jeremy Smith <st.jeremy1@gmail.com>
2023-02-06 11:43:17 +0100

    fix

--- Components/MovieCard.tsx
+++ Components/MovieCard.tsx
@@ -1,9 +1,17 @@
 import { Box } from "@chakra-ui/react";
 
+interface Image{
+  width: number,
+  height: number,
+  alt: string
+}
 const MovieCard = () => {
+  const url= 
+  "h"
   return ( 
     <Box>
-      <Image 
+      
+
     </Box>
    );
 }

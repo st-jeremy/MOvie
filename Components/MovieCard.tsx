d80b5b7e554b0826fbe9ce98a1be9fd96a89255b
Jeremy Smith <st.jeremy1@gmail.com>
2023-02-07 23:28:14 +0100

    updates

--- Components/MovieCard.tsx
+++ Components/MovieCard.tsx
@@ -1,4 +1,5 @@
 import { Box, Heading } from "@chakra-ui/react";
+import Image from "next/image";
 
 interface Image{
   width: number,
@@ -11,6 +12,7 @@ const MovieCard = ({movie}) => {
   return ( 
     <Box>
       <Heading>Movie: {movie.title}</Heading>
+      <Image src={"https://image.tmdb.org/t/p/w500" + movie.backdrop_path} alt={movie.path} />
 
     </Box>
    );

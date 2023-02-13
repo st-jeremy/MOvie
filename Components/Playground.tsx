555565cb2e205df57068a71c983de4e52ca86c48
Jeremy Smith <st.jeremy1@gmail.com>
2023-02-13 13:27:09 +0100

    fix

--- Components/Playground.tsx
+++ Components/Playground.tsx
@@ -1,9 +1,18 @@
 import { Box } from "@chakra-ui/react";
 
 const Playground = () => {
+  let cx = document.querySelectorAll("canvas").getContext("2d");
+cx.beginPath();
+cx.moveTo(10, 90);
+// control=(60,10) goal=(90,90)
+cx.quadraticCurveTo(60, 10, 90, 90);
+cx.lineTo(60, 10);
+cx.closePath();
+cx.stroke();
+
   return ( 
     <Box>
-      
+<canvas></canvas>
     </Box>
    );
 }

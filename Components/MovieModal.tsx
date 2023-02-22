9d1da3a174dd5ce7797e36a690b7738122a53eb8
Jeremy Smith <st.jeremy1@gmail.com>
2023-02-22 13:18:28 +0100

    updatesz

--- Components/MovieModal.tsx
+++ Components/MovieModal.tsx
@@ -1,11 +1,27 @@
 import { Box, Button } from "@chakra-ui/react";
+import { MovieSharp } from "@material-ui/icons";
 import Image from "next/image";
 
 const MovieModal = ({movieInfo, handleClose}) => {
   return ( 
     <Box>
-      <Image
-      <Button>Close</Button>
+      <Image src={movieInfo.Poster} height={ 100} width={ 70} alt={movieInfo.Poster} loading={"lazy"} />
+        <p>
+          <b>Actors:</b> {movieInfo.Actors}
+        </p>
+        <p>
+          <b>Genre:</b> {movieInfo.Genre}
+        </p>
+        <p>
+          <b>Director:</b> {movieInfo.Director}
+        </p>
+        <p>
+          <b>Released:</b> {movieInfo.Released}
+        </p>
+        <p>
+          <b>Plot:</b> {movieInfo.Plot}
+        </p>
+      <Button onClick={handleClose}>Close</Button>
     </Box>
    );
 }
